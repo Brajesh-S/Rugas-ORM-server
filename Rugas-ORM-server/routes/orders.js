eac2d29cb45b3b3638b5cff5245af3b8057e5a77
Brajesh-S <Brajesh-S@users.noreply.github.com>
2025-03-15 23:25:41 +0530

    nitpicks

--- Rugas-ORM-server/routes/orders.js
+++ Rugas-ORM-server/routes/orders.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Order = require('../models/Order');
+const Order = require('../models/order');
 const auth = require('../middleware/auth');
 
 router.post('/', auth, async (req, res) => {
