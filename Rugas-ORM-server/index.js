1de0f8f07cb070efb0bc093e62af80fcd7b78548
Brajesh-S <Brajesh-S@users.noreply.github.com>
2025-03-19 20:18:33 +0530

    nitpicks

--- Rugas-ORM-server/index.js
+++ Rugas-ORM-server/index.js
@@ -27,7 +27,6 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['set-cookie']
 }));
 app.use(bodyParser.json()); 
 app.use(cookieParser());
